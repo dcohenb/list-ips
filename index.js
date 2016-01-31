20cc9c495114dd7c2099338a7b732ac15317a1a5
Daniel Cohen <danielc@promots.tv>
2016-01-31 16:19:53 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,41 @@
+function list(range) {
+    var res = range.split('.').map(function(section) {
+        if (!isNaN(section)) {
+            return [parseInt(section)];
+        } else if (section.indexOf('-') !== -1) {
+            var r = section.split('-');
+            var n = parseInt(r[0]);
+            var m = parseInt(r[1]);
+            if (n > m) {
+                n = parseInt(r[1]);
+                m = parseInt(r[0]);
+            }
+            var a = [];
+            for (var i = n; i < m; i++) {
+                a.push(i);
+            }
+            return a;
+        } else if (section === "*") {
+            return Array.apply(null, {
+                length: 255
+            }).map(Number.call, Number);
+        }
+    })
+
+    var list = [];
+    res[0].forEach(function(a) {
+        res[1].forEach(function(b) {
+            res[2].forEach(function(c) {
+                res[3].forEach(function(d) {
+                    list.push([a, b, c, d].join('.'))
+                });
+            });
+        });
+    });
+    
+    return list;
+}
+
+return {
+    list: list
+}
